4b64b1b1ac5160eb5662072bedbceaf90b936564
Allan <allan.tuikong@student.moringaschool.com>
2022-08-20 15:46:54 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+// declare customerName to be bob in global scope
+var customerName = 'bob'
+var bestCustomer;
+const leastFavoriteCustomer = 'brrrr'
+
+//Write a function that accesses that global customerName variable, and uppercases it.
+
+function upperCaseCustomerName() {
+customerName = customerName.toUpperCase()
+return customerName
+};
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer
+};
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+};
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Oysusedgissfa'
+    return leastFavoriteCustomer
+};
